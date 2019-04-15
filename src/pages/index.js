706e423dc4f89127b76e728e875584f0ec0f8455
Kittinan <tunxedge@gmail.com>
2019-04-15 11:39:08 +0700

    finish tutorial 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Header headerText="Hello Gatsby!" />
+    <p>What a world.</p>
+    <Link to="/about/">About</Link>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </Layout>
+)
